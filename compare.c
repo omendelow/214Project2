278fdec6d47d4df209ecc8c1caac6d53582f57e0
orenmendelow <om144@scarletmail.rutgers.edu>
2021-04-07 14:02:42 -0400

    added c file

---
+++ compare.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/*
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <fcntl.h> 
+#include <assert.h>
+*/
+
+int main(int argc, char **argv) {
+	//do stuff
+}
